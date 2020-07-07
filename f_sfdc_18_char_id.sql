bafa284982c18423351caea71d92584f85d9a040
enece <53958496+enece@users.noreply.github.com>
2020-07-07 18:51:04 -0400

    Update f_sfdc_18_char_id.sql

--- f_sfdc_18_char_id.sql
+++ f_sfdc_18_char_id.sql
@@ -3,9 +3,9 @@ create or replace function f_sfdc_18_char_id(id varchar)
 stable
 as $$
   if len(id) == 18:
-    raise ValueError('The given id is already 18 characters long.')
+      raise ValueError('The given id is already 18 characters long.')
   if len(id) != 15:
-    raise ValueError('The given id is not 15 characters long.')
+      raise ValueError('The given id is not 15 characters long.')
   lookup_str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ012345'
   lookup_list = list(lookup_str)
   sum_list = [1, 2, 4, 8, 16]
@@ -13,12 +13,12 @@ as $$
   suffix = ''
 
   for i, char in enumerate(id):
-    sum_index = i % 5
-    if char.isupper():
-      counter += sum_list[sum_index]
-    if sum_index == 4:
-      suffix += lookup_list[counter]
-      counter = 0
+      sum_index = i % 5
+      if char.isupper():
+          counter += sum_list[sum_index]
+      if sum_index == 4:
+          suffix += lookup_list[counter]
+          counter = 0
 
   return id + suffix
 $$ language plpythonu
